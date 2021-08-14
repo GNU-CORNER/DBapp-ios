a84caa437b8a8e0f55053152c7f86b38196a74d3
황혁주 <71334624+nugulhie@users.noreply.github.com>
2021-08-14 22:08:45 +0900

    init project with StoryBoard

---
+++ DBapp-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DBapp-ios
+//
+//  Created by 황혁주 on 2021/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
